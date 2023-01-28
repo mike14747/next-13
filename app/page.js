7d743d916b04e904adc39a8fef359828bf7f2606
mike14747 <mike4747@oh.rr.com>
2023-01-28 12:54:07 -0500

    Added content to the homepage to wrote tests for.

--- app/page.js
+++ app/page.js
@@ -1,4 +1,11 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+
 export default function Page() {
+    const [showAdditional, setShowAdditional] = useState(false);
+
     return (
         <>
             <h2>Homepage</h2>
@@ -6,7 +13,21 @@ export default function Page() {
             <p>
                 This is the homepage in the app folder.
             </p>
-        </>
 
+            <p>
+                {showAdditional
+                    ? <button onClick={() => setShowAdditional(false)}>Hide Additional</button>
+                    : <button onClick={() => setShowAdditional(true)}>Show Additional</button>
+                }
+            </p>
+
+            {showAdditional &&
+                <p>
+                    <Link href="/page1">
+                        This will only be shown when enabled.
+                    </Link>
+                </p>
+            }
+        </>
     );
 }
