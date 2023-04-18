3822f1da1e1a1bc29d97ef56df7a3b802d9109a2
mike14747 <mike4747@oh.rr.com>
2023-04-18 15:55:32 -0400

    Testing a new getServerSession method in the test page.

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../api/auth/[...nextauth]/route';
 
 export const metadata: Metadata = {
     title: 'next-13 - Test page',
 };
 
 export default async function Directory() {
-    const session = await getServerSession({
-        callbacks: { session: ({ token }) => token },
-    });
+    // const session = await getServerSession({
+    //     callbacks: { session: ({ token }) => token },
+    // });
+
+    const session = await getServerSession(authOptions);
 
     if (!session) {
         redirect('/login?callbackUrl=/test');
